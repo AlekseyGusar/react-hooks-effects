415d05165afb8830c7f3bc4936e7bd1916750d9e
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-05-17 21:15:17 +0300

    add hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 export default function App() {
+  const [number, setNumber] = useState('');
+  useEffect(() => {
+    setNumber(localStorage.getItem('appData'));
+  }, [])
 
   return (
     <div>
-      React Marathon, appData: <input size='5'></input>
+      React Marathon, appData: <input size='5' defaultValue={number}></input>
     </div>
   );
 }
